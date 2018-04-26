b0f64c2d4ab06e79efcc1d185d9efb3273af8f4b
kalyanogila <kalyanreddyogila@gmail.com>
2018-04-26 18:11:38 +0530

    Update SearchResultsTest.java

--- src/com/amazon/tests/SearchResultsTest.java
+++ src/com/amazon/tests/SearchResultsTest.java
@@ -77,7 +77,7 @@ public class SearchResultsTest extends BaseTest {
 	
 	@BeforeMethod
 	public void searchBooks() {
-		homePage.searchText("search-alias=stripbooks", "dataCatalog");
+		homePage.searchText("search-alias=stripbooks", "data Catalog");
 		bookDetailsPage=homePage.clickOnBook("Catalog It!: A Guide to Cataloging School Library Materials, 3rd Edition");
 	}
 	
